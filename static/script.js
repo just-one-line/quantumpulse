10d38b7cdf7301d6cce52448ebe4606de33f8cb6
just-one-line <admin@just-one-line.com>
2025-08-20 01:05:56 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,8 @@
-// script.js
-
+// static/script.js
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("JavaScript loaded!");
-
-  // Example: simple button click handler
-  const button = document.getElementById("actionButton");
-  if (button) {
-    button.addEventListener("click", () => {
-      alert("Button was clicked!");
-    });
+  const toggle = document.querySelector("[data-menu-toggle]");
+  const list = document.querySelector("nav ul");
+  if (toggle && list) {
+    toggle.addEventListener("click", () => list.classList.toggle("open"));
   }
 });
